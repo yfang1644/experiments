a7c742d3148ca281be22309e5c5a8ac6f1348888
yfang1644 <yfang1644@gmail.com>
2016-09-10 17:24:45 +0800

    add files

---
+++ experiments/beaglebone/gtk_dependency.mm
@@ -0,0 +1,72 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1473312342091" ID="ID_869660490" MODIFIED="1473318822942" STYLE="bubble" TEXT="gtk">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1473312371193" ID="ID_1521695683" MODIFIED="1473313112148" POSITION="right" STYLE="bubble" TEXT="pango">
+<edge COLOR="#111111" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1473312387504" ID="ID_8295090" MODIFIED="1473318862865" STYLE="bubble" TEXT="cairo">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1473312461896" ID="ID_580972686" MODIFIED="1473313056659" STYLE="bubble" TEXT="pixman">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1473312499096" ID="ID_167829036" MODIFIED="1473318858824" STYLE="bubble" TEXT="DirectFB">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1473312507488" ID="ID_174937415" MODIFIED="1473313056659" STYLE="bubble" TEXT="tslib">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1473312620985" ID="ID_138512137" MODIFIED="1473318773422" STYLE="bubble" TEXT="libpng">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1473312869467" ID="ID_41988051" MODIFIED="1473313056659" STYLE="bubble" TEXT="zlib">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1473312634617" ID="ID_896916526" MODIFIED="1473313056659" STYLE="bubble" TEXT="libjpeg">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1473312626225" ID="ID_693755026" MODIFIED="1473313056660" STYLE="bubble" TEXT="freetype">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1473312413848" ID="ID_967520163" MODIFIED="1473313056660" STYLE="bubble" TEXT="fontconfig">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1473312658858" ID="ID_1828092106" MODIFIED="1473313056660" STYLE="bubble" TEXT="freetype">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1473312662617" ID="ID_144421611" MODIFIED="1473334799697" STYLE="bubble" TEXT="libxml2">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1473312391216" ID="ID_1213698361" MODIFIED="1473313056660" STYLE="bubble" TEXT="glib">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1473312643153" ID="ID_423876399" MODIFIED="1473313056660" STYLE="bubble" TEXT="dbus">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1473312652465" ID="ID_1527804913" MODIFIED="1473313056660" STYLE="bubble" TEXT="expat">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1473312647689" ID="ID_647599537" MODIFIED="1473313056660" STYLE="bubble" TEXT="zlib">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1473312377400" ID="ID_526823867" MODIFIED="1473313087239" POSITION="right" STYLE="bubble" TEXT="atk">
+<edge COLOR="#111111" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1473312381624" ID="ID_1811719950" MODIFIED="1473313056660" STYLE="bubble" TEXT="glib">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
